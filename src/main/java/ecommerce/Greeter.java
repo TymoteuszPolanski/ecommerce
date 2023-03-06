42aef44110e83a089adac8866a0da19334f7e451
TymoteuszPolanski <s222733@student.uek.krakow.pl>
2023-03-06 12:49:28 +0100

    plays with functional api

---
+++ src/main/java/ecommerce/Greeter.java
@@ -0,0 +1,7 @@
+package ecommerce;
+
+public class Greeter {
+    public void greet(String names) {
+        System.out.println("Hello, " + names + "!");
+    }
+}
--- src/main/java/ecommerce/Main.java
+++ src/main/java/ecommerce/Main.java
@@ -1,7 +1,28 @@
 package ecommerce;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello there");
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Tymek", "Michał", "Jakub", "Agnieszka", "Maciej");
+
+        Greeter greeter = new Greeter();
+        greeter.greet("Jakub");
+        List<String> ladies = new ArrayList<String>();
+        for (String name : names){
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+        for (String ladyName: ladies) {
+            greeter.greet(ladyName);
+        }
+
+        names.stream()
+                .filter(name -> name.endsWith("a"))
+                .forEach(greeter::greet);
     }
 }
